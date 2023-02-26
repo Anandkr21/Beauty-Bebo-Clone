f67bec730a63f085027c1c0f23e57da2d30b0991
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-02-26 14:24:33 +0530

    admin page created

---
+++ frontend/scripts/admin.js
@@ -0,0 +1,53 @@
+let title = document.getElementById("title")
+let image_1 = document.getElementById("Image_1")
+let price = document.getElementById("Price")
+let Category = document.getElementById("Category")
+let Errors = document.getElementById("Errors")
+let added = document.getElementById("added")
+let submit = document.getElementById("submit")
+
+
+let myform = document.querySelector("form");
+
+myform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (title.value == "" || image_1.value == "" || price.value == "" || Category.value == "") {
+        
+        Errors.style.display = "inline"
+        setTimeout(() => {
+            Errors.style.display = "none"
+        }, 2000)
+    } else {
+        added.style.display = "inline"
+        setTimeout(() => {
+            added.style.display = "none"
+        }, 2000)
+        let obj = {
+            title: title.value,
+            image_1: image_1.value,
+            price: price.value,
+            Category: Category.value,
+        }
+        addProduct(obj);
+    }
+
+})
+
+function addProduct(obj){
+    fetch("https://colambia-api.onrender.com/AllProduct",{
+        method : "POST",
+        headers: {
+            "Content-Type": "application/json",
+          },
+        body : JSON.stringify(obj)
+    })
+}
+let logout=document.getElementById("out")
+let log=document.getElementById("logout");
+log.addEventListener("click",()=>{
+    logout.style.display = "inline"
+    setTimeout(() => {
+        logout.style.display = "none";
+        window.open("./index.html")
+    }, 2000)
+})

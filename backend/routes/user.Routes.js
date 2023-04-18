66c7edb0352b59aecf83a3a8424f6b5f46b7df70
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-04-19 02:37:11 +0530

    logout section complete

--- backend/routes/user.Routes.js
+++ backend/routes/user.Routes.js
@@ -1,55 +1,60 @@
-const express=require('express')
-const jwt=require('jsonwebtoken')
-const bcrypt=require('bcrypt')
-const {userModel}=require('../model/users.model')
-const userRouter=express.Router()
+const express = require('express')
+const fs = require('fs')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const { userModel } = require('../model/users.model')
+const userRouter = express.Router()
 
 // User Signup
-userRouter.post('/register', async(req,res) =>{
-    const{name,email,gender,pass,age,city}= req.body;
+userRouter.post('/register', async (req, res) => {
+    const { name, email, gender, pass, age, city } = req.body;
     try {
-        bcrypt.hash(pass,3, (err,hash) =>{
-            if(err){
-                res.send({'msg':'something wrong', 'error':err.message})
-            }else{
-                const user=new userModel({name,email,gender,age,city,pass:hash})
+        bcrypt.hash(pass, 3, (err, hash) => {
+            if (err) {
+                res.send({ 'msg': 'something wrong', 'error': err.message })
+            } else {
+                const user = new userModel({ name, email, gender, age, city, pass: hash })
                 user.save()
-                res.send({'msg':'Registered'})
+                res.send({ 'msg': 'Registered' })
             }
         })
     } catch (err) {
-        res.send({'msg':'somthing went wrong', 'error':err.message})
+        res.send({ 'msg': 'somthing went wrong', 'error': err.message })
     }
 })
 
 // User Login 
-userRouter.post('/login', async(req,res) =>{
-    const {email,pass} =(req.body)
+userRouter.post('/login', async (req, res) => {
+    const { email, pass } = (req.body)
     try {
-        const user = await userModel.find({email})
-        if(user.length>0){
-            bcrypt.compare(pass,user[0].pass, (err,result) =>{
+        const user = await userModel.find({ email })
+        if (user.length > 0) {
+            bcrypt.compare(pass, user[0].pass, (err, result) => {
                 if (result) {
-                    let token=jwt.sign({userid:user[0]._id}, 'anand')
-                    res.send({'msg':'Login Successfully', 'token':token})
+                    let token = jwt.sign({ userid: user[0]._id }, 'anand')
+                    res.send({ 'msg': 'Login Successfully', 'token': token })
                 } else {
-                    res.send({"msg":'something wrong'})
+                    res.send({ "msg": 'something wrong' })
                 }
             })
-        }else{
-            res.send({"msg":'something wrong'})
+        } else {
+            res.send({ "msg": 'something wrong' })
         }
     } catch (err) {
-        res.send({"msg":'something wrong', 'error':err.message})
+        res.send({ "msg": 'something wrong', 'error': err.message })
     }
 })
 
 
 // User Logout
-userRouter.post('/logout', (req,res) =>{
+userRouter.get('/logout', (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1]
+    const blacklist_data = JSON.parse(fs.readFileSync('./blacklist.json', 'utf-8'))
+    blacklist_data.push(token)
+    fs.writeFileSync('./blacklist.json', JSON.stringify(blacklist_data))
     res.send('You are Successfully Logout')
 })
 
-module.exports={
+module.exports = {
     userRouter
 }
